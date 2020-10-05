adef4d3418ed44e0eb94f489cf37e591069b6e1a
rileyseaburg <riley@DESKTOP-MQ1N2PR.localdomain>
2020-10-05 08:42:10 -0400

    fix typo and add cart functionality

--- frontend/src/pages/cart.js
+++ frontend/src/pages/cart.js
@@ -1,22 +1,53 @@
 import React from 'react'
-import { getCart } from '../utils/cart'
+import { getCart, addToCart } from '../utils/cart'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Img from 'gatsby-image'
+import { formatPrice } from '../utils/format'
 export default () => {
     const cart = getCart()
     return (
-       <Layout>
-           <SEO title={cart} />
-           <div>
-               {cart.map(product => (
-                   <div>
-                       {/* <Img fixed={} /> */}
-                       <h3>Name</h3>
-                       <p>Price</p>
-                   </div>
-               ))}
-           </div>
-       </Layout>
+        <Layout>
+            <SEO title={cart} />
+            <h2>Cart</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            Product
+                        </th>
+                        <th>
+                            Price
+                        </th>
+                        <th>
+                            Quantity
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cart.map(product => (
+                        <tr>
+                            <td>
+                                <Img
+                                    style={{
+                                        width: '150px',
+                                        height: '150px'
+                                    }}
+                                    fixed={product.thumbnail.childImageSharp.fixed} />
+                                <span>{product.name}</span>
+                            </td>
+                            <td>
+                                <p>{formatPrice(product.price_in_cents)}</p>
+                            </td>
+                            <td>
+                                <span style={{cursor: 'pointer'}} onClick={() => addToCart(product, -1)} > - </span>
+                                {product.qty}
+                                <span style={{cursor: 'pointer'}} onClick={() => addToCart(product, 1)} > + </span>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </Layout>
     )
 }
--- frontend/src/utils/cart.js
+++ frontend/src/utils/cart.js
@@ -1,10 +1,12 @@
 export const setCart = (cart) => {
     localStorage.setItem('cart', JSON.stringify(cart))
 }
-
+/**
+ * function getCart
+ */
 export const getCart = () => {
     try {
-        const cart = JSON.Parse(localStorage.getItem('cart'))
+        const cart = JSON.parse(localStorage.getItem('cart'))
         if (cart) {
             return cart
         }
@@ -13,8 +15,12 @@ export const getCart = () => {
     }
     return []
 }
-
-export const addToCart = (product) => {
+/**
+ * 
+ * @param {*} product 
+ * @param {*} qty 
+ */
+export const addToCart = (product, qty = 1) => {
     const cart = getCart()
     // If the product is already there
     const indexOfProduct = cart.findIndex((alreadyInCart) =>
@@ -24,11 +30,29 @@ export const addToCart = (product) => {
     if (indexOfProduct !== -1) {
         // Update the quantity
         cart[indexOfProduct].qty += 1
+
+        if (cart[indexOfProduct].qty === 0) {
+            // Remove the product from the cart
+            cart.splice(indexOfProduct, 1)
+        }
     } else {
         // Set the qty 1
         product.qty = 1
         // Push the product
-        cart.push(product)
+        cart.push(product)  
     }
     setCart(cart)
+  
 }
+
+// export const updateProductQuantity = (product, quantity) {
+//     const cart = getCart()
+
+//     const indexOfProduct = cart.findIndex((alreadyInCart) => 
+//         alreadyInCart.strapiId === product.strapiId
+//     )
+
+//     if (indexOfProduct) {
+        
+//     }
+// } 
